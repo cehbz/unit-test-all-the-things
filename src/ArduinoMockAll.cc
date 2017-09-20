96359e40b79a761b65bfcf1d501b2b1e1d4a6552
Charles Haynes <ceh+github@ceh.bz>
2017-09-20 19:37:15 +1000

    add gmock/gtest files

---
+++ src/ArduinoMockAll.cc
@@ -0,0 +1,13 @@
+// Copyright 2015 http://switchdevice.com
+
+#include "Arduino.cc"
+#include "EEPROM.cc"
+#include "Serial.cc"
+#include "serialHelper.cc"
+#include "Spark.cc"
+#include "WiFi.cc"
+#include "Wire.cc"
+#include "SPI.cc"
+#include "DHT.cc"
+
+#include "IRremote.cc"
